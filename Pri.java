2a0690aa942cf53f3676793db3b02a5aab74c55f
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-03-11 17:13:49 +0530

    Rename Pos.java to Pri.java

---
+++ Pri.java
@@ -0,0 +1,23 @@
+package priya;
+
+import java.util.Scanner;
+
+public class PosNeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a>0) {
+    	System.out.println("Positive");
+    }
+    else if(a<0) {
+    	System.out.println("Negative");
+    }
+    else {
+    	System.out.println("Zero");
+    }
+    
+	}
+
+}
