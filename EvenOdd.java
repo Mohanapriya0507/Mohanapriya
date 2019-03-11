b08ed563cdaace99aad3237421664afdfd904f29
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-03-11 17:18:35 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,26 @@
+package priya;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Scanner sc=new Scanner(System.in);
+  int a=sc.nextInt();
+  if(a>0) {
+  if(a%2==0) {
+	  System.out.println("Even");
+  }
+  else if(a%2!=0) {
+	  
+	  System.out.println("Odd");
+  }
+  
+	}
+  else {
+	  System.out.println("Invalid");
+  }
+}
+}
+	 
