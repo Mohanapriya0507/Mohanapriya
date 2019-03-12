c904f3b1bbc7b33985edebb116f5af0b52acf92b
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-03-12 17:41:21 +0530

    Create VowCon.java

---
+++ VowCon.java
@@ -0,0 +1,22 @@
+package priya;
+
+import java.util.Scanner;
+
+public class Vowel{
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    char ch=sc.next().charAt(0);
+    if(!Character.isDigit(ch)&&Character.isAlphabetic(ch)) {
+    	System.out.println("Invalid");
+    }
+    else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+    	System.out.println("Vowels");
+    }
+    else {
+    	System.out.println("Consonants");
+    }
+    
+	}
+
+}
