1e1b19b63d27801b50f608e4d87159a243609b62
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-06-15 09:43:20 +0530

    Create HoursMin.java

---
+++ HoursMin.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HoursMin {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int hours=t/60;
+        int minutes=t%60;
+        System.out.println(hours+" "+minutes);
+    }
+}
