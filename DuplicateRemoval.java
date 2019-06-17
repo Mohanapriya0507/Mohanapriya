f7813055fc3c9116042b9cc87eaeab46ec2fef3a
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-06-17 10:32:05 +0530

    Create DuplicateRemoval.java

---
+++ DuplicateRemoval.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class DuplicateRemoval{
+public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++){
+a[i]=scan.nextInt();
+}
+Arrays.sort(a);
+
+for(int i=0;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(a[i]==a[j]){
+System.out.println(a[i]);
+}
+else{
+System.out.println("unique");
+}
+
+}
+}
+}
