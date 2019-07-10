c896da30423ecdbeabd32e7fe6ecc3bc821b594e
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-07-10 13:51:55 +0530

    Create StringKTimes.java

---
+++ StringKTimes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class StringKTimes {
+
+	public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int k=sc.nextInt();
+     String s=sc.next();
+     for(int i=0;i<k;i++){
+    	 System.out.println(s);
+     }
+
+	}
+
+}
