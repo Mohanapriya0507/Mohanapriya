124a8df65111fe0662c8d26fcca5a668a8c716d4
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-07-10 14:20:44 +0530

    Create Range.java

---
+++ Range.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Range {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>=1 && n<=10){
+     System.out.println("yes");
+	}
+	else {
+		System.out.println("No");
+	}
+}
+	}
+
+
