4c67b6ac7c3aee024482465227185ee44b674eda
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-07-10 14:14:32 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Concat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            Scanner sc=new Scanner(System.in);
+            String s=sc.next();
+            String s1=sc.next();
+            System.out.println(s+s1);
+ 
+	}
+
+}
