1cbf470378a85fd50a2f8086ba0c626ff5d47796
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-07-10 16:10:39 +0530

    Create One1.java

---
+++ One1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class One1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+switch(n){
+case 1:
+System.out.println("one");
+break;
+case 2:
+	System.out.println("two");
+	break;
+default :
+	System.out.println("invalid");
+}
+
+	}
+
+}
