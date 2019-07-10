ca0c837e28171f34028ba41a13d644910470a662
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-07-10 14:33:18 +0530

    Create  CountDigits.java

---
+++ CountDigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class CountDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        int m=n.length();
+        System.out.println(m);
+        
+	}
+
+}
