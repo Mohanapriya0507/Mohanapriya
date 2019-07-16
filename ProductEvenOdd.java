5747487e2e358d01771c7adc5f768815f4791b88
Mohanapriya0507 <43246605+Mohanapriya0507@users.noreply.github.com>
2019-07-16 15:05:24 +0530

    Create ProductEvenOdd.java

---
+++ ProductEvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ProductEvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		int product=m*n;
+		if(product%2==0){
+			System.out.println("even");
+		}
+		else{
+		}
+	}
+
+}
